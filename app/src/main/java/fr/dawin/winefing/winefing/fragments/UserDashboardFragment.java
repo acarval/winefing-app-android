71a4338c12c2c1eb525322754ecdbd8f449b096d
vmorreel <dawin.vmorreel@gmail.com>
2017-02-06 11:05:32 +0100

    Refactor structure du projet

--- app/src/main/java/fr/dawin/winefing/winefing/fragments/UserDashboardFragment.java
+++ app/src/main/java/fr/dawin/winefing/winefing/fragments/UserDashboardFragment.java
@@ -1,4 +1,4 @@
-package fr.dawin.winefing.winefing;
+package fr.dawin.winefing.winefing.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
@@ -6,6 +6,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import fr.dawin.winefing.winefing.R;
 
 /**
  * Created by vmorreel on 01/02/2017.
