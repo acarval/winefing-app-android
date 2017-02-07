6ebd0ecb22131737707480545be5aaab923d74d8
vmorreel <dawin.vmorreel@gmail.com>
2017-02-07 11:30:27 +0100

    #23:Vue d'une Propriété donnée:Ajout d'un timer pour défilement auto des images de la propriétés dans le slideshow:(v.morreel)

--- app/src/main/java/fr/dawin/winefing/winefing/fragments/LocationsFromPropertyFragment.java
+++ app/src/main/java/fr/dawin/winefing/winefing/fragments/LocationsFromPropertyFragment.java
@@ -2,12 +2,18 @@ package fr.dawin.winefing.winefing.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringDef;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import fr.dawin.winefing.winefing.R;
 import fr.dawin.winefing.winefing.adapters.AndroidImageAdapter;
@@ -19,14 +25,67 @@ import fr.dawin.winefing.winefing.adapters.AndroidImageAdapter;
 public class LocationsFromPropertyFragment extends Fragment {
     View myView;
 
+    private static final String TAG = "LocationsPropertyFrag";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_locations_from_property, container, false);
 
-        ViewPager mViewPager = (ViewPager) myView.findViewById(R.id.property_slideshow);
-        AndroidImageAdapter adapterView = new AndroidImageAdapter(this.getActivity());
+        final ViewPager mViewPager = (ViewPager) myView.findViewById(R.id.property_slideshow);
+        final AndroidImageAdapter adapterView = new AndroidImageAdapter(this.getActivity());
+
+
+
         mViewPager.setAdapter(adapterView);
+
+       /* final Handler handler = new Handler();
+
+        final Runnable Update = new Runnable() {
+            public void run() {
+                for (int i = 0; i < adapterView.getCount(); i++){
+                    mViewPager.setCurrentItem(i, true);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.e(TAG, String.valueOf(i));
+                }
+            }
+        };
+
+        Timer swipeTimer = new Timer();
+
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 5000, 5000);*/
+
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < adapterView.getCount(); i++) {
+                    final int value = i;
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mViewPager.setCurrentItem(value, true);
+                        }
+                    });
+                    Log.e(TAG, String.valueOf(i));
+                }
+            }
+        };
+        new Thread(runnable).start();
 
         return myView;
     }
